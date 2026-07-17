8160303e35479fedb28a4470eb07cedd9f8555b4
great-special <sirgreatspecial@gmail.com>
2026-07-17 08:52:42 +0100

    fix: Update ALLOWED_HOSTS to specify the correct render URL

--- rubysite/settings.py
+++ rubysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-p3#gvwx0#ct9vy@3aw&u@+fn0-ra3dweq(6#pqr(vqtft(%8w_
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'http://*.onrender.com',
+    'http://ruby-d4hk.onrender.com',
     'http://127.0.0.1:8000/',
     'http://localhost:8000/',
     'http://rubytechlearning.com'
