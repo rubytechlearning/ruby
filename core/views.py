736561bb5b5b37062fc522fbf774067a1fb1f839
great-special <sirgreatspecial@gmail.com>
2026-08-13 14:31:47 +0100

    feat: Limit courses displayed on the index page to 6

--- core/views.py
+++ core/views.py
@@ -11,7 +11,7 @@ from .models import ContactLeads, Testimonials, Partner
 
 def index(request):
     categories = Category.objects.all()[:6]  # Limit to 6 categories
-    courses = Course.objects.all()
+    courses = Course.objects.all()[:6]  # Limit to 6 courses
     testimonials = Testimonials.objects.all()[:4]
     instructors = Instructor.objects.all()[:6]
     partners = Partner.objects.all()[:6]
